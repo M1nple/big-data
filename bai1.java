23a56e8d9f7bcbe56e8ef52d683d287e09d00972
M1nple <ngocminh652003@gmail.com>
2024-11-02 02:35:55 +0700

    init

---
+++ bai1.java
@@ -0,0 +1,74 @@
+D:\Workspace\dữ liệu lớn và ứng dụngimport java.util.;
+import java.io.IOException;
+import java.io.IOException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.;
+import org.apache.hadoop.io.;
+import org.apache.hadoop.mapred.;
+import org.apache.hadoop.util.;
+
+public class BT1 {
+	public static class E_EMapper extends MapReduceBase
+	implements
+	 MapperLongWritable ,Input key Type 
+	 Text, Input value Type
+	 Text, Output key Type
+	 IntWritable Output value Type
+	 {
+	 Map function
+	 public void map(LongWritable key, Text value,
+	 OutputCollectorText, IntWritable output,
+
+	 Reporter reporter) throws IOException {
+	 String line = value.toString();
+	 String lasttoken = null;
+	 StringTokenizer s = new StringTokenizer(line,t);
+	 String year = s.nextToken();
+
+	 while(s.hasMoreTokens()) {
+	 lasttoken = s.nextToken(); 
+	 }
+	 int avgprice = Integer.parseInt(lasttoken);
+	 output.collect(new Text(year), new
+	IntWritable(avgprice));
+	 }
+	 }
+
+	 Reducer class
+	 public static class E_EReduce extends MapReduceBase
+	implements Reducer Text, IntWritable, Text, IntWritable  {
+
+	 Reduce function
+	 public void reduce( Text key, Iterator IntWritable
+	values,
+	 OutputCollectorText, IntWritable output, Reporter
+	reporter) throws IOException {
+	 int maxavg = 30;
+	 int val = Integer.MIN_VALUE;
+
+	 while (values.hasNext()) {
+	 if((val = values.next().get())maxavg) {
+	 output.collect(key, new IntWritable(val));
+	 }
+	 }
+	 }
+	 }
+	 Main function
+	 public static void main(String args[])throws Exception {
+	 JobConf conf = new JobConf(BT1.class);
+
+	 conf.setJobName(max_eletricityunits); 
+	 conf.setOutputKeyClass(Text.class);
+	 conf.setOutputValueClass(IntWritable.class);
+	 conf.setMapperClass(E_EMapper.class);
+	 conf.setCombinerClass(E_EReduce.class);
+	 conf.setReducerClass(E_EReduce.class);
+	 conf.setInputFormat(TextInputFormat.class);
+	 conf.setOutputFormat(TextOutputFormat.class);
+
+	 FileInputFormat.setInputPaths(conf, new Path(args[0]));
+	 FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+	 JobClient.runJob(conf);
+	 }
+}
